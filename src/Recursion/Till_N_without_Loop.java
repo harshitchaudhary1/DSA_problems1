4fde6e172cb44ce8e5d611bfecdf90919cc69696
Harshit Chaudhary <83287475+harshitchaudhary1@users.noreply.github.com>
2023-12-30 11:00:16 +0530

    recursion update

---
+++ src/Recursion/Till_N_without_Loop.java
@@ -0,0 +1,23 @@
+package Recursion;
+import java.util.Scanner;
+
+public class Till_N_without_Loop {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		
+		int x = sc.nextInt();
+		int[] arr = new int[x];
+		
+		PrintNTimes(x, arr);
+		
+	}
+	public static void PrintNTimes(int x , int[] arr) {
+		if(x>=1) {
+			arr[x-1]=x;
+			PrintNTimes(x-1, arr);
+			
+		}
+		System.out.println(arr);
+	}
+}
