162ccb1fc99ce0af208658b9f67a1d0daba5585f
Harshit Chaudhary <83287475+harshitchaudhary1@users.noreply.github.com>
2024-02-22 22:18:32 +0530

    commit

---
+++ src/udemy_learning/Multipliaction_Table.java
@@ -0,0 +1,14 @@
+package udemy_learning;
+
+public class Multipliaction_Table {
+	public static void print() {
+		for(int i =0;i<10;i++) {
+			System.out.printf("%d * %d = %d", 5, i,5*i);
+			System.out.println();
+		}
+		
+	}
+	public static void main(String[] args) {
+	print();
+	}
+}
