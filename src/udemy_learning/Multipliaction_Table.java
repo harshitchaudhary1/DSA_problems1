81064bfe099a93a15d62e9332dfaa3e0d8c0592d
Harshit Chaudhary <83287475+harshitchaudhary1@users.noreply.github.com>
2024-02-23 01:09:07 +0530

    update

--- src/udemy_learning/Multipliaction_Table.java
+++ src/udemy_learning/Multipliaction_Table.java
@@ -1,14 +1,24 @@
 package udemy_learning;
 
 public class Multipliaction_Table {
-	public static void print() {
-		for(int i =1	;i<10;i++) {
+	 void print() {
+		for(int i =1;i<=10;i++) {
 			System.out.printf("%d * %d = %d", 5, i,5*i);
 			System.out.println();
-		}
-		
+		}	
 	}
-	public static void main(String[] args) {
-	print();
+	 void print(int num) {
+		for(int i=1;i<=10;i++) {
+			System.out.printf("%d * %d =%d", num,i,num*i);
+			System.out.println();
+
+		}
+	}
+	 void print(int num,int from, int to) {
+		for(int i=from;i<=to;i++) {
+			System.out.printf("%d * %d =%d", num,i,num*i);
+			System.out.println();
+		
+		}
 	}
 }
