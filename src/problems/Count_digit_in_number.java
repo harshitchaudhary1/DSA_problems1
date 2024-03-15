bb7b622e5a462e6da18da289e281fc283a9328bb
Harshit Chaudhary <83287475+harshitchaudhary1@users.noreply.github.com>
2024-03-15 22:26:14 +0530

    update

--- src/problems/Count_digit_in_number.java
+++ src/problems/Count_digit_in_number.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 public class Count_digit_in_number {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a Digit");
-		int n = sc.nextInt();
-		
-		int count=0;
-		
-		while(n!=0) {
-			n=n/10;
-			++count;
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter a Digit");
+			int n = sc.nextInt();
+			
+			int count=0;
+			
+			while(n!=0) {
+				n=n/10;
+				++count;
+			}
+			System.out.println(count);
 		}
-		System.out.println(count);
 	}
 }
