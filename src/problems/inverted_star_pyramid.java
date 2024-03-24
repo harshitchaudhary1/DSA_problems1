28bb83945e751a944714799fe336a394bb690581
Harshit Chaudhary <83287475+harshitchaudhary1@users.noreply.github.com>
2024-03-24 23:21:42 +0530

    commit

--- src/problems/inverted_star_pyramid.java
+++ src/problems/inverted_star_pyramid.java
@@ -5,7 +5,7 @@ public class inverted_star_pyramid {
 	public static void main(String[] args) {
 		try (Scanner sc = new Scanner(System.in)) {
 			int n = sc.nextInt();
-			// Star pyramind pattern 
+
 			for(int i=0;i<n;i++) {
 				for(int j=0;j<i;j++) {
 					System.out.print(" ");
